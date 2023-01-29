06c992bd66f3b0699c2a3187c41ffe7ad55436f3
Godo Sanchez Heredia <gssanchezh@ieee.org>
2023-01-29 16:46:23 -0500

    Update Config.h

--- Ejemplo0_FC.X/Config.h
+++ Ejemplo0_FC.X/Config.h
@@ -21,10 +21,10 @@ void WriteData(void);
 uint8_t HTTP_Post(void);
 
 /* SIN RETORNO - CON ENTRADA DE PARAMETROS*/
-
+void HMI_WRITE_DATA(int *ID,char *string,uint8_t X_pos,uint8_t Y_pos);
 
 /* CON RETORNO - CON ENTRADA DE PARAMETROS*/
-
+float PID_Calculo(float *Planta,float ref,float feedback);
 
 #endif	/* CONFIG_H */
 
